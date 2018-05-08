fc30f4e61d15648065457f7c668f7d6b71c59ed1
Omarbek <dinasil.omarbek@gmail.com>
2018-05-08 19:21:31 +0600

    edited user arrival as client said

---
+++ src/main/java/kz/halyqsoft/univercity/entity/beans/univercity/catalog/TURNSTILE_TYPE.java
@@ -0,0 +1,11 @@
+package kz.halyqsoft.univercity.entity.beans.univercity.catalog;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Omarbek
+ * @created on 08.05.2018
+ */
+@Entity
+public class TURNSTILE_TYPE extends AbstractTypeEntity {
+}
