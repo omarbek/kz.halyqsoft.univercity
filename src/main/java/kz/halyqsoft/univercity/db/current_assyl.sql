2cbd209071343e2bcce80ec815cbd2b103724e04
Omarbek <dinasil.omarbek@gmail.com>
2018-06-25 15:17:57 +0600

    merged with master

--- src/main/java/kz/halyqsoft/univercity/db/current_assyl.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current_assyl.sql
@@ -51,39 +51,6 @@ create sequence S_MESSAGE
   start with 1
   no cycle;
 
--- GROUPS
-
-CREATE TABLE groups (
-  id  BIGINT  NOT NULL,
-  speciality_id BIGINT NOT NULL,
-  name CHARACTER VARYING(150) NOT NULL,
-  orders BIGINT
-);
-
-ALTER TABLE groups
-  ADD CONSTRAINT pk_group PRIMARY KEY (id);
-
-ALTER TABLE ONLY groups
-  ADD CONSTRAINT fk_group_speciality FOREIGN KEY (speciality_id)
-REFERENCES speciality (id)
-ON UPDATE RESTRICT ON DELETE RESTRICT;
-
-create sequence S_GROUPS
-  minvalue 0
-  start with 1
-  no cycle;
-
-ALTER TABLE groups ADD COLUMN deleted BOOLEAN NOT NULL ;
-
-ALTER TABLE groups ADD COLUMN created TIMESTAMP DEFAULT now();
-
-ALTER TABLE student_education ADD COLUMN groups_id BIGINT;
-
-ALTER TABLE ONLY student_education
-  ADD CONSTRAINT fk_student_education_groups FOREIGN KEY (groups_id)
-REFERENCES groups (id)
-ON UPDATE RESTRICT ON DELETE RESTRICT;
-
 -- DOCUMENT_ACTIVITY
 
 CREATE TABLE DOCUMENT_ACTIVITY (
