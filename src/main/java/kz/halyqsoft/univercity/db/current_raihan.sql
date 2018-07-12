815187e08de495150d47eac6ea639790d4e51ee7
Raihan Abdyzhalilova <xxx.raihan97@gmail.com>
2018-07-12 15:25:45 +0600

    fixed bugs with restarting

--- src/main/java/kz/halyqsoft/univercity/db/current_raihan.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current_raihan.sql
@@ -23,3 +23,35 @@ update users set created_by = 'admin' where created_by is null;
 
 alter table users
   alter column created_by set not null;
+
+ALTER TABLE elective_binded_subject DROP COLUMN first_subject_id;
+
+ALTER TABLE elective_binded_subject DROP COLUMN second_subject_id;
+
+CREATE TABLE pair_subject(
+  id                         BIGINT  NOT NULL,
+  subject_id                 BIGINT  NOT NULL,
+  elective_binded_subject_id BIGINT  NOT NULL,
+  pair_number                BIGINT  NOT NULL
+);
+
+CREATE SEQUENCE s_pair_subject
+  minvalue 0
+ start with 1
+  no cycle;
+
+ALTER TABLE pair_subject
+  ADD CONSTRAINT pk_pair_subject PRIMARY KEY (id);
+
+ALTER TABLE ONLY pair_subject
+  ADD CONSTRAINT fk_pair_subject_subject FOREIGN KEY (subject_id)
+REFERENCES subject (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+ALTER TABLE ONLY pair_subject
+  ADD CONSTRAINT fk_pair_subject_elective_binded_subject FOREIGN KEY (elective_binded_subject_id)
+REFERENCES elective_binded_subject (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+CREATE UNIQUE index idx_elective_binded_subject
+  ON elective_binded_subject (semester_id, catalog_elective_subjects_id);
