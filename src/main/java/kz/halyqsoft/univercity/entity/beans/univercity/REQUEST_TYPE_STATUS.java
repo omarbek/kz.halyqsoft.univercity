8668d4b7fdea728c68209f41c9d9b326c5cfade2
Raihan Abdyzhalilova <xxx.raihan97@gmail.com>
2018-08-01 18:49:58 +0600

    entity class for request_type_status

---
+++ src/main/java/kz/halyqsoft/univercity/entity/beans/univercity/REQUEST_TYPE_STATUS.java
@@ -0,0 +1,12 @@
+package kz.halyqsoft.univercity.entity.beans.univercity;
+
+import kz.halyqsoft.univercity.entity.beans.univercity.catalog.AbstractStatusEntity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class REQUEST_TYPE_STATUS extends AbstractStatusEntity {
+
+    public REQUEST_TYPE_STATUS() {
+    }
+}
