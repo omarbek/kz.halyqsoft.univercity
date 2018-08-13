90801ddd917b4447de724edb46b25bba2121885c
damira <damirazoldasbay@gmail.com>
2018-08-13 11:28:16 +0600

    Added magister(master) tableWidget

--- src/main/java/kz/halyqsoft/univercity/db/current_damira.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current_damira.sql
@@ -60,4 +60,70 @@ CREATE OR REPLACE VIEW V_EMPLOYEE_DEGREE AS
 
 ALTER TABLE employee_degree RENAME COLUMN "school_name" TO "place_of_issue";
 
+_____________________________________________________________________________
 
+
+CREATE TABLE master(
+  id BIGINT NOT NULL,
+  entrance_year DATE NOT NULL ,
+  graduation_year DATE NOT NULL ,
+  university_id BIGINT NOT NULL,
+  speciality_id BIGINT NOT NULL,
+  qulification_id BIGINT NOT NULL,
+  diploma_number BIGINT NOT NULL,
+  employee_id BIGINT NOT NULL,
+  deleted BOOLEAN NOT NULL
+);
+
+CREATE SEQUENCE s_master
+  MINVALUE 0
+  START WITH 1
+  NO CYCLE;
+
+ALTER TABLE master
+  ADD CONSTRAINT pk_master PRIMARY KEY (id);
+
+ALTER TABLE ONLY master
+  ADD CONSTRAINT fk_master_university FOREIGN KEY (university_id)
+REFERENCES university (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+ALTER TABLE master
+  ADD CONSTRAINT fk_master_speciality FOREIGN KEY (speciality_id)
+REFERENCES speciality (id)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE master
+  ADD CONSTRAINT fk_master_qualification FOREIGN KEY (qulification_id)
+REFERENCES qualification (id)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE master
+  ADD CONSTRAINT fk_master_employee FOREIGN KEY (employee_id)
+REFERENCES users (id)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+CREATE UNIQUE INDEX idx_master ON master USING btree
+(university_id,speciality_id,qulification_id) where deleted = false;
+
+CREATE OR REPLACE VIEW V_MASTER AS
+SELECT
+  m.id,
+  m.entrance_year,
+  m.graduation_year,
+  m.university_id,
+  u.university_name UNIVERSITY_NAME,
+  m.speciality_id,
+  s.spec_name SPECIALITY_NAME,
+  m.qulification_id,
+  q.qualification_name QUALIFICATION_NAME,
+  m.diploma_number,
+  m.employee_id,
+  e.id EMPLOYEE,
+  m.deleted
+FROM master m
+  INNER JOIN university u ON m.speciality_id=u.id
+  INNER JOIN speciality s ON m.speciality_id=s.id
+  INNER JOIN qualification q ON m.qulification_id=q.id
+  INNER JOIN users e ON m.employee_id=e.id
+WHERE m.deleted = FALSE ;
