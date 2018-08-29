f546623e3f4b3b883c646f6a17f3c9d14ed60f2c
Omarbek <dinasil.omarbek@gmail.com>
2018-08-29 16:22:46 +0600

    moved scripts from current_assyl and current_damira to current

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -478,3 +478,149 @@ CREATE OR REPLACE VIEW V_TEACHER_LOAD_ASSIGN_DETAIL AS
     load_assign_det.semester_id, sem.semester_name, cred.CREDIT, academ_form.FORMULA,
     load_assign_det.SEMESTER_PERIOD_ID, sem_period.PERIOD_NAME, load_assign_det.LC_HOUR,
     load_assign_det.LB_HOUR, load_assign_det.PR_HOUR;
+
+--assyl
+-- NON ADMISSION
+
+CREATE TABLE non_admission_cause (
+  id BIGINT NOT NULL ,
+  name VARCHAR(255) NOT NULL
+);
+
+ALTER TABLE non_admission_cause ADD CONSTRAINT pk_non_admission_cause PRIMARY KEY (id);
+
+CREATE SEQUENCE s_non_admission_cause
+MINVALUE 0
+start WITH 1
+no CYCLE ;
+
+CREATE TABLE non_admission_exam (
+  id BIGINT NOT NULL ,
+  student_id BIGINT NOT NULL,
+  non_admission_cause_id BIGINT NOT NULL,
+  semester_data_id BIGINT NOT NULL,
+  created TIMESTAMP NOT NULL DEFAULT now()
+);
+
+ALTER TABLE non_admission_exam ADD CONSTRAINT pk_non_admission_exam PRIMARY KEY (id);
+
+CREATE SEQUENCE s_non_admission_exam
+MINVALUE 0
+START WITH 1
+NO CYCLE ;
+
+ALTER TABLE non_admission_exam ADD CONSTRAINT fk_non_admission_exam_student
+FOREIGN KEY (student_id) REFERENCES student(id)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+ALTER TABLE non_admission_exam ADD CONSTRAINT fk_non_admission_exam_semester_data
+FOREIGN KEY (semester_data_id) REFERENCES semester_data(id)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT;
+
+ALTER TABLE non_admission_exam ADD CONSTRAINT fk_non_admission_exam_non_admission_cause
+FOREIGN KEY (non_admission_cause_id) REFERENCES non_admission_cause(id)
+ON DELETE RESTRICT
+ON UPDATE RESTRICT ;
+
+
+CREATE TABLE non_admission_subject (
+  id BIGINT NOT NULL ,
+  non_admission_exam_id BIGINT NOT NULL,
+  subject_id BIGINT NOT NULL
+);
+
+ALTER TABLE non_admission_subject ADD CONSTRAINT pk_non_admission_subject
+FOREIGN KEY (non_admission_exam_id) REFERENCES non_admission_exam(id);
+
+CREATE SEQUENCE s_non_admission_subject
+MINVALUE 0
+START WITH 1
+NO CYCLE ;
+
+ALTER TABLE non_admission_subject ADD CONSTRAINT fk_non_admission_subject_non_admission_exam
+FOREIGN KEY (non_admission_exam_id) REFERENCES non_admission_exam(id)
+ON UPDATE RESTRICT ON DELETE RESTRICT ;
+
+ALTER TABLE non_admission_subject ADD CONSTRAINT fk_non_admission_subject_subject
+FOREIGN KEY (subject_id) REFERENCES subject(id)
+ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+DROP VIEW v_groups_creation_needed;
+
+CREATE VIEW v_groups_creation_needed
+  AS (select
+        grs.id,
+        s2.id speciality_id,
+        grs.language_id ,
+        vs.entrance_year_id,
+        grs.study_year_id,
+        c2.corpus_id
+      FROM groups grs
+        INNER JOIN speciality s2
+          ON grs.speciality_id = s2.id
+        INNER JOIN speciality_corpus c2
+          ON s2.id = c2.speciality_id
+        INNER JOIN v_student vs
+          ON vs.groups_id = grs.id
+      WHERE
+        grs.deleted = FALSE
+        and s2.deleted = FALSE);
+
+ALTER TABLE groups ALTER COLUMN study_year_id DROP NOT NULL ;
+
+UPDATE tasks SET name = 'KK=Жатақхана;RU=Общежитие;EN=Dorm;', title = 'KK=Жатақхана;RU=Общежитие;EN=Dorm;', task_type = false, task_order = 221, class_path = 'kz.halyqsoft.univercity.modules.dorm.DormView', parent_id = 3, icon_name = null, descr = 'KK=Жатақхана;RU=Общежитие;EN=Dorm;', visible = true WHERE id = 44;
+
+-- INSERT INTO student_status VALUES (nextval('s_student_status' ), 'Оставлен на перекурс');
+
+ALTER TABLE user_arrival ADD COLUMN manually_signed BOOLEAN NULL DEFAULT FALSE;
+
+--damira
+CREATE VIEW v_student_debts AS
+  SELECT
+    s2.id,
+    x.user_code,
+    trim(x.LAST_NAME || ' ' || x.FIRST_NAME || ' ' || coalesce(x.MIDDLE_NAME, '')) fio,
+    CASE WHEN x.id = payment.student_id
+      THEN (a.price - (sum(payment.payment_sum)))
+    ELSE a.price END                                                               DEBT_SUM
+  FROM v_student x
+    INNER JOIN student s2 ON x.id = s2.id
+    INNER JOIN student_diploma_type s3 ON s2.diploma_type_id = s3.id
+    INNER JOIN accountant_price a ON s3.id = a.student_diploma_type_id
+    LEFT JOIN student_payment AS payment ON payment.student_id = s2.id
+  WHERE a.contract_payment_type_id = 2 AND
+        a.level_id = x.level_id AND x.deleted = FALSE AND a.deleted = FALSE
+  GROUP BY payment.student_id, s2.id, x.user_code, x.last_name, x.first_name, x.middle_name, x.id, a.price;
+
+ALTER TABLE EMPLOYEE_DEPT
+  ADD priority BOOLEAN NOT NULL DEFAULT FALSE;
+
+DROP VIEW V_EMPLOYEE_DEPT;
+
+CREATE OR REPLACE VIEW V_EMPLOYEE_DEPT AS
+  SELECT
+    empl_dept.ID,
+    empl_dept.EMPLOYEE_ID,
+    empl_dept.EMPLOYEE_TYPE_ID,
+    empl_type.TYPE_NAME EMPLOYEE_TYPE_NAME,
+    empl_dept.DEPT_ID,
+    dep.DEPT_NAME,
+    dep.DEPT_SHORT_NAME,
+    empl_dept.POST_ID,
+    post.POST_NAME,
+    empl_dept.LIVE_LOAD,
+    empl_dept.WAGE_RATE,
+    empl_dept.RATE_LOAD,
+    empl_dept.HOUR_COUNT,
+    empl_dept.HIRE_DATE,
+    empl_dept.DISMISS_DATE,
+    empl_dept.ADVISER,
+    empl_dept.PARENT_ID,
+    empl_dept.lecturer,
+    empl_dept.priority
+  FROM EMPLOYEE_DEPT empl_dept INNER JOIN EMPLOYEE_TYPE empl_type ON empl_dept.EMPLOYEE_TYPE_ID = empl_type.ID
+    INNER JOIN DEPARTMENT dep ON empl_dept.DEPT_ID = dep.ID
+    LEFT JOIN POST post ON empl_dept.POST_ID = post.ID
+    INNER JOIN EMPLOYEE empl ON empl_dept.EMPLOYEE_ID = empl.ID;
