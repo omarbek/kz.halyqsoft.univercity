9d2d94f6e6b7a308e4e1bffe0dd51390c18c72b9
Omarbek <dinasil.omarbek@gmail.com>
2018-09-11 11:22:56 +0600

    moved scripts from current_assyl to current

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -79,3 +79,54 @@ ALTER TABLE ONLY subject
   ADD CONSTRAINT fk_practice_type FOREIGN KEY (practice_type_id)
 REFERENCES practice_type (id)
 ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+-- CHAT
+
+CREATE TABLE chat (
+  id             BIGINT NOT NULL,
+  first_user_id  BIGINT NOT NULL,
+  second_user_id BIGINT NOT NULL,
+  accepted       BOOLEAN
+);
+
+ALTER TABLE chat
+  ADD CONSTRAINT pk_chat PRIMARY KEY (id);
+
+ALTER TABLE ONLY chat
+  ADD CONSTRAINT fk_chat_users_first FOREIGN KEY (first_user_id)
+REFERENCES users (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+
+ALTER TABLE ONLY chat
+  ADD CONSTRAINT fk_chat_users_second FOREIGN KEY (second_user_id)
+REFERENCES users (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+CREATE SEQUENCE S_CHAT
+MINVALUE 0
+START WITH 1
+NO CYCLE;
+
+-- MESSAGE
+
+CREATE TABLE message (
+  id         BIGINT    NOT NULL,
+  chat_id    BIGINT    NOT NULL,
+  content    TEXT,
+  created    TIMESTAMP NOT NULL,
+  from_first BOOLEAN   NOT NULL
+);
+
+ALTER TABLE message
+  ADD CONSTRAINT pk_message PRIMARY KEY (id);
+
+ALTER TABLE ONLY message
+  ADD CONSTRAINT fk_message_chat FOREIGN KEY (chat_id)
+REFERENCES chat (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+CREATE SEQUENCE S_MESSAGE
+MINVALUE 0
+START WITH 1
+NO CYCLE;
