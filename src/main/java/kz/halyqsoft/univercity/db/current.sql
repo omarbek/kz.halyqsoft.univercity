4c0cf0cda932beee838aaf2e4fd7ccd6d48549d9
Omarbek <dinasil.omarbek@gmail.com>
2018-09-11 11:17:47 +0600

    moved scripts from current_raihan to current

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -60,3 +60,22 @@ VALUES
 INSERT INTO nationality
 VALUES
   (nextval('s_nationality') , 'Араб');
+
+CREATE TABLE practice_type(
+  id        BIGINT       NOT NULL,
+  type_name VARCHAR(128) NOT NULL
+);
+
+ALTER TABLE practice_type
+  ADD CONSTRAINT pk_practice_type PRIMARY KEY (id);
+
+INSERT INTO practice_type VALUES(1,'Производственная');
+INSERT INTO practice_type VALUES(2,'Учебная');
+INSERT INTO practice_type VALUES(3,'Педагогическая');
+
+ALTER TABLE subject ADD practice_type_id int null;
+
+ALTER TABLE ONLY subject
+  ADD CONSTRAINT fk_practice_type FOREIGN KEY (practice_type_id)
+REFERENCES practice_type (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
