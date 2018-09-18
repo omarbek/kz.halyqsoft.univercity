a015d17f5757a6ec4a7f060c61e20a6567486e58
Omarbek <dinasil.omarbek@gmail.com>
2018-09-18 09:16:02 +0600

    moved scripts from current_raihan to current

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -6,3 +6,13 @@ ALTER TABLE pair_subject
   ADD COLUMN aim BIGINT NULL;
 ALTER TABLE pair_subject
   ADD COLUMN competence BIGINT NULL;
+
+ALTER TABLE pair_subject ADD COLUMN code BIGINT NULL;
+
+ALTER TABLE ONLY pair_subject
+  ADD CONSTRAINT fk_pair_subject_postrequisite FOREIGN KEY (postrequisite_id)
+REFERENCES subject (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
+
+ALTER TABLE ONLY pair_subject
+  ADD CONSTRAINT fk_pair_subject_prerequisite FOREIGN KEY (prerequisite_id)
+REFERENCES subject (id) ON UPDATE RESTRICT ON DELETE RESTRICT;
