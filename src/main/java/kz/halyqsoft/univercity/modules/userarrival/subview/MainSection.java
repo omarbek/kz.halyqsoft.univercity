c1be36ec4be0a40f0f087b704968b71e894655fe
air <asilkhan-al@mail.ru>
2018-09-19 17:38:18 +0600

    STATISTIC corrected

--- src/main/java/kz/halyqsoft/univercity/modules/userarrival/subview/MainSection.java
+++ src/main/java/kz/halyqsoft/univercity/modules/userarrival/subview/MainSection.java
@@ -56,27 +56,11 @@ public class MainSection implements FilterPanelListener {
 
     private StatisticsFilterPanel statisticsFilterPanel;
 
-    private Long allUsers = 0L;
-    private Long allStudents = 0L;
-    private Long allEmployees = 0L;
     private ChartJs chart;
 
     public MainSection() {
 
         BarChartConfig barConfig = new BarChartConfig();
-        barConfig.
-                data()
-                .labels()
-                .addDataset(
-                        new BarDataset().backgroundColor("rgba(220,220,220,0.5)").label("Dataset 1").yAxisID("y-axis-1"))
-                .addDataset(
-                        new BarDataset().backgroundColor("rgba(151,187,205,0.5)").label("Dataset 2").yAxisID("y-axis-2").hidden(true))
-                .addDataset(
-                        new BarDataset().backgroundColor(
-                                ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7),
-                                ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7),
-                                ColorUtils.randomColor(0.7)).label("Dataset 3").yAxisID("y-axis-1"))
-                .and();
         barConfig.
                 options()
                 .responsive(true)
@@ -87,23 +71,12 @@ public class MainSection implements FilterPanelListener {
                 .and()
                 .title()
                 .display(true)
-                .text("Chart.js Bar Chart - Multi Axis")
+                .text(CommonUtils.getUILocaleUtil().getCaption("attendance"))
                 .and()
                 .scales()
                 .add(Axis.Y, new LinearScale().display(true).position(Position.LEFT).id("y-axis-1"))
-                .add(Axis.Y, new LinearScale().display(true).position(Position.RIGHT).id("y-axis-2").gridLines().drawOnChartArea(false).and())
                 .and()
                 .done();
-
-        List<String> labels = barConfig.data().getLabels();
-        for (Dataset<?, ?> ds : barConfig.data().getDatasets()) {
-            BarDataset lds = (BarDataset) ds;
-            List<Double> data = new ArrayList<>();
-            for (int i = 0; i < labels.size(); i++) {
-                data.add((Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100));
-            }
-            lds.dataAsList(data);
-        }
 
         chart = new ChartJs();
         chart.setJsLoggingEnabled(true);
@@ -605,13 +578,34 @@ public class MainSection implements FilterPanelListener {
         List<SimpleStatistics> simpleStatistics = new ArrayList<>();
         if(fStatisticsFilter.getDepartment()!= null && fStatisticsFilter.getEndingDate()!=null && fStatisticsFilter.getStartingDate()!=null ){
             simpleStatistics.addAll(getStatisticList(fStatisticsFilter.getDepartment().getId().getId().longValue(), fStatisticsFilter.getStartingDate(), fStatisticsFilter.getEndingDate()));
+            refresh(simpleStatistics);
         }
-        refresh(simpleStatistics);
     }
 
     private void refresh(List<SimpleStatistics> list) {
+
         BarChartConfig barConfig = (BarChartConfig) chart.getConfig();
-        //TODO
+        barConfig.data().clear();
+        barConfig.data().labels(CommonUtils.getUILocaleUtil().getCaption("attendance"));
+
+        barConfig.
+                data()
+                .labels(CommonUtils.getUILocaleUtil().getCaption("attendance"));
+        for(SimpleStatistics ss : list){
+            barConfig.data()
+                    .addDataset(
+                            new BarDataset().backgroundColor(ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7), ColorUtils.randomColor(0.7)).label(ss.getGroupName()).yAxisID("y-axis-1"));
+        }
+
+        List<String> labels = barConfig.data().getLabels();
+        for (int j = 0 ; j < barConfig.data().getDatasets().size() ; j++) {
+            BarDataset lds = (BarDataset) barConfig.data().getDatasets().get(j);
+            List<Double> data = new ArrayList<>();
+            for (int i = 0; i < labels.size(); i++) {
+                data.add(list.get(j).getPercentage());
+            }
+            lds.dataAsList(data);
+        }
         chart.configure(barConfig);
         chart.update();
     }
@@ -625,6 +619,6 @@ public class MainSection implements FilterPanelListener {
 
     @Override
     public void clearFilter() {
-        refresh(new ArrayList());
+
     }
 }
