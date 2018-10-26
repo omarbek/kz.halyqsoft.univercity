247df6a894a7c647594fb85eba9958ee2a96a8c8
Omarbek <dinasil.omarbek@gmail.com>
2018-10-26 16:44:41 +0600

    edited showing groups in manual to 200;
    fixed showing user arrival in groups;
    
    fixed document ids;
    
    created refresh button in common utils.

--- src/main/java/kz/halyqsoft/univercity/utils/DocumentIDs.java
+++ src/main/java/kz/halyqsoft/univercity/utils/DocumentIDs.java
@@ -1,15 +1,15 @@
 package kz.halyqsoft.univercity.utils;
 
 public class DocumentIDs {
-    public final static String IUPS_KAZ_ID = "97";
-    public final static String IUPS_RUS_ID = "96";
-    public final static String DORM_CONTRACT_KAZ_ID = "92";
-    public final static String CONTRACT_RUS_ID = "85";
+    public final static String OTINISH_OKPU_ID = "27";
     public final static String TITLE_ID = "32";
     public final static String KOLKHAT_ID = "33";
-    public final static String MAGIS_CONTRACT_ID = "82";
-    public final static String KELISIM_SHART_ID = "90";
+    public final static String MAGIS_CONTRACT_ID = "82";//???
+    public final static String CONTRACT_RUS_ID = "85";
     public final static String OTINISH_ID = "89";
-    public final static String OTINISH_OKPU_ID = "27";
+    public final static String KELISIM_SHART_ID = "90";
+    public final static String DORM_CONTRACT_KAZ_ID = "92";//???
+    public final static String IUPS_RUS_ID = "98";
+    public final static String IUPS_KAZ_ID = "99";
 
 }
