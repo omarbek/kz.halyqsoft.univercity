990d83f04881e86a8f35d029e8d8d5436d92a79b
Omarbek <dinasil.omarbek@gmail.com>
2018-11-06 17:42:18 +0600

    moved damira's scripts to current;

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -199,3 +199,20 @@ ALTER TABLE curriculum_add_program
   ALTER COLUMN code DROP NOT NULL;
 ALTER TABLE curriculum_add_program
   ALTER COLUMN education_module_type_id DROP NOT NULL;
+
+--teacher subject
+CREATE OR REPLACE VIEW V_TEACHER_SUBJECT_MODULE AS
+  SELECT
+    teacher_subject.ID,
+    teacher_subject.EMPLOYEE_ID,
+    subj.NAME_RU SUBJECT_NAME,
+    credit.credit,
+    subjm.module_name,
+    teacher_subject.FALL,
+    teacher_subject.SPRING,
+    teacher_subject.SUMMER,
+    teacher_subject.LOAD_PER_HOURS
+  FROM TEACHER_SUBJECT teacher_subject
+    INNER JOIN SUBJECT subj ON teacher_subject.SUBJECT_ID = subj.ID
+    INNER JOIN creditability credit ON subj.creditability_id = credit.id
+    INNER JOIN subject_module subjm ON subj.module_id = subjm.id;
