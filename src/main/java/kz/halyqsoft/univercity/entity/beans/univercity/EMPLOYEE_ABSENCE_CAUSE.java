b0e2455b541c63fa2cd940fbc4071287e30cd8f9
air <asilkhan-al@mail.ru>
2018-11-21 12:13:23 +0600

    SQL tablefunc extension

--- src/main/java/kz/halyqsoft/univercity/entity/beans/univercity/EMPLOYEE_ABSENCE_CAUSE.java
+++ src/main/java/kz/halyqsoft/univercity/entity/beans/univercity/EMPLOYEE_ABSENCE_CAUSE.java
@@ -1,6 +1,8 @@
 package kz.halyqsoft.univercity.entity.beans.univercity;
 
+import kz.halyqsoft.univercity.entity.beans.univercity.catalog.ABSENCE_CAUSE;
 import kz.halyqsoft.univercity.entity.beans.univercity.catalog.ATTESTATION_TYPE;
+import kz.halyqsoft.univercity.entity.beans.univercity.view.V_EMPLOYEE;
 import org.r3a.common.entity.AbstractEntity;
 import org.r3a.common.entity.EFieldType;
 import org.r3a.common.entity.FieldInfo;
@@ -15,35 +17,75 @@ import java.util.Date;
 @Entity
 public class EMPLOYEE_ABSENCE_CAUSE extends AbstractEntity {
 
-	private static final long serialVersionUID = 5436195498756128045L;
-
 	@FieldInfo(type = EFieldType.FK_COMBO, order = 1)
     @ManyToOne
     @JoinColumns({
-        @JoinColumn(name = "SCHEDULE_DETAIL_ID", referencedColumnName = "ID")})
-    private SCHEDULE_DETAIL scheduleDetail;
+        @JoinColumn(name = "EMPLOYEE_ID", referencedColumnName = "ID")})
+    private V_EMPLOYEE employee;
 
 	@FieldInfo(type = EFieldType.FK_COMBO, order = 2)
     @ManyToOne
     @JoinColumns({
-        @JoinColumn(name = "ATTESTATION_TYPE_ID", referencedColumnName = "ID")})
-    private ATTESTATION_TYPE attestationType;
+        @JoinColumn(name = "ABSENCE_CAUSE_ID", referencedColumnName = "ID")})
+    private ABSENCE_CAUSE absenceCause;
 
 
-	@FieldInfo(type = EFieldType.DATE, order = 4)
+	@FieldInfo(type = EFieldType.DATE, order = 3)
 	@Column(name = "STARTING_DATE")
     @Temporal(TemporalType.TIMESTAMP)
     private Date startingDate;
 
-	@FieldInfo(type = EFieldType.DATE, order = 5)
+	@FieldInfo(type = EFieldType.DATE, order = 4)
 	@Column(name = "FINAL_DATE")
     @Temporal(TemporalType.TIMESTAMP)
     private Date finalDate;
 
 
-	@FieldInfo(type = EFieldType.DATE, order = 6)
+	@FieldInfo(type = EFieldType.DATE, order = 5, inEdit = false, inView = false, inGrid = false)
 	@Column(name = "created")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date created = new Date();
 
+	public EMPLOYEE_ABSENCE_CAUSE() {
+	}
+
+	public V_EMPLOYEE getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(V_EMPLOYEE employee) {
+		this.employee = employee;
+	}
+
+	public ABSENCE_CAUSE getAbsenceCause() {
+		return absenceCause;
+	}
+
+	public void setAbsenceCause(ABSENCE_CAUSE absenceCause) {
+		this.absenceCause = absenceCause;
+	}
+
+	public Date getStartingDate() {
+		return startingDate;
+	}
+
+	public void setStartingDate(Date startingDate) {
+		this.startingDate = startingDate;
+	}
+
+	public Date getFinalDate() {
+		return finalDate;
+	}
+
+	public void setFinalDate(Date finalDate) {
+		this.finalDate = finalDate;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
 }
