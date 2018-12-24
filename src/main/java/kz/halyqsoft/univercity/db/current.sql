d9d145101f3de7b0b0f4df1d77b5fc917691d03c
omarbek <adibek.t@maint.kz>
2018-12-24 15:02:36 +0600

    moved scripts

--- src/main/java/kz/halyqsoft/univercity/db/current.sql
+++ src/main/java/kz/halyqsoft/univercity/db/current.sql
@@ -623,3 +623,25 @@ CREATE OR REPLACE VIEW V_LOAD_TO_CHAIR_WORK AS
     AND curr.deleted = FALSE
     AND sem.study_year_id = gr.study_year_id
     and spec.chair_id = subj.chair_id;
+
+ALTER TABLE schedule_detail DROP COLUMN stream_id;
+
+UPDATE degree
+SET degree_name = 'кандидат наук'
+WHERE id = 1;
+UPDATE degree
+SET degree_name = 'доктор наук'
+WHERE id = 2;
+UPDATE degree
+SET degree_name = 'доктор Ph.D'
+WHERE id = 3;
+UPDATE degree
+SET degree_name = 'ассоциированный профессор (доцент)'
+WHERE id = 4;
+
+INSERT INTO DEGREE (DEGREE_NAME, ID)
+VALUES ('профессор', 7);
+INSERT INTO DEGREE (DEGREE_NAME, ID)
+VALUES ('доцент', 8);
+INSERT INTO DEGREE (DEGREE_NAME, ID)
+VALUES ('профессоров (по новой квалификации)', 9);
